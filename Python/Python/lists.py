1e0177fddaff05d54f429a1e1b94c58ee84f01a7
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-12 10:16:38 +0000

    Lists_banana_pizza

---
+++ Python/Python/lists.py
@@ -0,0 +1,36 @@
+numbers = [1, 2, 3]
+print(numbers)
+print(numbers[0])
+
+pizza_toppings = ["cheese", "pepperoni"]
+
+print(pizza_toppings)
+print(len(pizza_toppings))
+
+pizza_toppings.append("pineappple")
+print(pizza_toppings)
+
+pizza_toppings.insert(0, "banana")
+print(pizza_toppings)
+
+del pizza_toppings[1]
+print(pizza_toppings)
+
+pizza_toppings.pop()
+print(pizza_toppings)
+
+pizza_toppings.remove("banana")
+print(pizza_toppings)
+
+
+
+mixed_list = [1, "chesse", 2, "pepperoni"]
+print(mixed_list)
+
+
+
+list_of_lists = [[1, 2], [2, 3]]
+print(list_of_lists[0])
+print(list_of_lists[1][1]) # 3
+
+
