cf7af81af03a6ee1a44fa8a3320fe09070266640
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-12 10:58:57 +0000

    if_statement

---
+++ Python/Python/if_statement.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers_greater_than_seven = []
+
+for i in numbers:
+    if i > 7:
+        numbers_greater_than_seven.append(i)
+
+print(numbers_greater_than_seven)
+
