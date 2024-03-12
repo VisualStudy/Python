4de8f87f1763c6d8581de8b892a72631c5021bc7
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-12 12:10:32 +0000

    function_test

---
+++ Python/Python/function_test.py
@@ -0,0 +1,17 @@
+hamburgers = ["cheese", "bulgogi"]
+
+def format_ham(ham):
+    if ham == "cheese":
+        return f"{ham.title()} (Free)"
+    else:
+        return f"{ham.title()} ($1 Extra)"
+    
+
+
+def print_menu(hams):
+    print("Welcome to visual studyria")
+    print("Our available hams are:")
+    for ham in hams:
+        print(format_ham(ham))
+        
+print_menu(hamburgers)
