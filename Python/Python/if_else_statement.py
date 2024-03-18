4c5870ea05361605cda8d2682596a504a3df9869
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-18 15:27:17 +0000

    Code Cleanup

--- Python/Python/if_else_statement.py
+++ Python/Python/if_else_statement.py
@@ -1,8 +1,9 @@
 pizza_toppings = ["cheese", "pepperoni", "banana"]
+
 print("We have the following toppings:")
+
 for topping in pizza_toppings:
     if topping == "cheese":
         print(f"{topping} (free)")
     else:
         print(f"{topping} ($1.00)")
-        
