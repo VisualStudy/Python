246927925e245a8fcb19a09b23709cd471ced40c
VisualStudy <161845684+VisualStudy@users.noreply.github.com>
2024-03-18 15:28:15 +0000

    Run Code Cleanup

--- Python/Python/lists.py
+++ Python/Python/lists.py
@@ -22,15 +22,9 @@ print(pizza_toppings)
 pizza_toppings.remove("banana")
 print(pizza_toppings)
 
-
-
 mixed_list = [1, "chesse", 2, "pepperoni"]
 print(mixed_list)
-
-
 
 list_of_lists = [[1, 2], [2, 3]]
 print(list_of_lists[0])
 print(list_of_lists[1][1]) # 3
-
-
