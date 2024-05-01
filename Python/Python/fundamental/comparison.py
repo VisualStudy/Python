dede28b8fa74dd175f7d6c888d031846e4506d3c
VisualStudy <sparksoldier@outlook.com>
2024-05-01 23:27:55 +0900

    .

--- Python/Python/fundamental/comparison.py
+++ Python/Python/fundamental/comparison.py
@@ -13,3 +13,5 @@ data >= myData    # data가 myData보다 크거나 같은가? output : False
 data < myData     # data가 myData보다 작은가? output : True
 
 data <= myData    # data가 myData보다 작거나 같은가? : True
+
+#비교 연산자는 숫자 뿐만 아니라 문자열도 비교할 수 있다. 문자끼리 비교할 때는 아스키코드를 따른다.
