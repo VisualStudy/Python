c9a4349d40429cb6744ee65ea798191bf4d62eeb
VisualStudy <sparksoldier@outlook.com>
2024-05-01 23:20:00 +0900

    assignment

---
+++ Python/Python/fundamental/assignment.py
@@ -0,0 +1,15 @@
+data = 10
+
+data += 2   # data = data + 2              output : 12
+
+data -= 2   # data = data - 2              output : 8
+
+data *= 2   # data = data * 2              output : 20
+
+data /= 2   # data = data / 2              output : 5.0 (³ª´°¼ÀÀº Ç×»ó ½Ç¼öÇü)
+
+data %= 2   # data = data % 2              output : 0
+
+data //= 3  # data = data // 3             output : 3 (data¸¦ 3À¸·Î ³ª´« ¸òÀ» data¿¡ ´ëÀÔÇÑ´Ù.) 
+
+data **= 3  # data = data * data * data    output : 1000
