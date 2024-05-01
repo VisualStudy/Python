818d0113e606648fd79b3c0d9eeafcf194a0e038
VisualStudy <sparksoldier@outlook.com>
2024-05-01 23:26:40 +0900

    comparison

---
+++ Python/Python/fundamental/comparison.py
@@ -0,0 +1,15 @@
+data = 10
+myData = 30
+
+
+data == myData    # 둘이 같은가? 같으면 참, 그렇지 않다면 거짓 output : False
+
+data != myData    # 둘이 다른가? 다르면 참, 그렇지 않다면 거짓 output : True
+
+data > myData     # data가 myData보다 큰가? output : False
+
+data >= myData    # data가 myData보다 크거나 같은가? output : False
+
+data < myData     # data가 myData보다 작은가? output : True
+
+data <= myData    # data가 myData보다 작거나 같은가? : True
