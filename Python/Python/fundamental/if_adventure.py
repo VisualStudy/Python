4f7a11ec25fa1eb12ddf20da5fc55bcc3138881c
VisualStudy <sparksoldier@outlook.com>
2024-05-03 22:37:03 +0900

    .

--- Python/Python/fundamental/if_adventure.py
+++ Python/Python/fundamental/if_adventure.py
@@ -21,6 +21,7 @@ if yourAlarm == 1:
         print('저런! 너무 오래 자서 오전 11시가 되었습니다!')
         print('')
         print('')
+        print('')
         print('GAME OVER')
     
     elif moreSleep == 2:
