06619213b7123f370eaeff20d2eea7f020e1b3e0
VisualStudy <sparksoldier@outlook.com>
2024-05-11 20:45:59 +0900

    .

--- Python/Python/fundamental/for_multiplication_table.py
+++ Python/Python/fundamental/for_multiplication_table.py
@@ -3,7 +3,7 @@
 num = int(input('원하는 단을 입력하시오 :  '))
 
 for var in range(1, 10, 1):
-   print(num, end = ' ')
+   print(num, end = ' ') # end=''으로 print문 출력 이어 붙이기
    print('*', end = ' ')
    print(var, end = ' ')
    print('=', end = ' ')
