607a2dfaa6c1a1077b1d241ea15ad485a86eb412
VisualStudy <sparksoldier@outlook.com>
2024-05-18 21:32:21 +0900

    .

--- Python/Python/fundamental/while.py
+++ Python/Python/fundamental/while.py
@@ -1,5 +1,5 @@
 ﻿num = 1 # 초기값
 
 while num <= 30: # num이 10보다 작거나 같을 때
-    print(num)   # num값 출력
-    num = num + 1     # 무한 루프 방지
+    print(num, end = ' ')   # num값 출력, end = ' '로 줄바꿈 없이 이어 붙이되, 한 칸 띄어 구분하기
+    num = num + 1     # 무한 루프 방지 // num += 1과 동일
