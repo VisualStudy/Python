1d1cfd56110aa07fa04004934ff5e337ac238f76
VisualStudy <sparksoldier@outlook.com>
2024-06-20 22:53:25 +0900

    refactoring

--- Python/Python/CW/test_input.py
+++ Python/Python/CW/test_input.py
@@ -1,5 +1,5 @@
 ﻿your_name = input('당신의 이름 : ')
 your_int = int(input('당신이 지닌 정수 : '))
 
-print('당신의 이름은'+your_name+'입니다')
+print('당신의 이름은',your_name,'입니다')
 print('당신이 지닌 정수에 1을 더한 값은',your_int + 1,'입니다')
