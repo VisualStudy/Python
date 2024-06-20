6d8a006acaa1bed9609327ea0a72fb6f7cc48a9b
VisualStudy <sparksoldier@outlook.com>
2024-06-20 18:04:10 +0900

    .

---
+++ Python/Python/CW/test.py
@@ -0,0 +1,10 @@
+myMoney = 5000000
+rate = 0.05
+
+myMoney += myMoney * rate
+myMoney += myMoney * rate
+myMoney += myMoney * rate
+myMoney += myMoney * rate
+myMoney += myMoney * rate
+
+print('5년 후 총 수령액 : ', int(myMoney), '원')
