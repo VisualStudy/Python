b785f2007199d4f7d3534bde2f4e4341d7ca9b2e
VisualStudy <sparksoldier@outlook.com>
2024-06-22 23:08:36 +0900

    .

--- Python/Python/global_function.py
+++ Python/Python/global_function.py
@@ -1,7 +1,7 @@
-def add_two_number(a,b):
+def add_two_number(a, b):
     #sum_result는 지역 변수
     sum_result = a + b
     return sum_result
 
-result = add_two_number(3,5)
+result = add_two_number(3, 5)
 print("두 수의 합 :", result)
