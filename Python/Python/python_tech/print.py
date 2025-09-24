488560e2cbbdaea5ada79005273b3aa864eeb95f
지호 박 <sparksoldier@outlook.com>
2025-09-24 21:51:44 +0900

    oh my rabarata

--- Python/Python/python_tech/print.py
+++ Python/Python/python_tech/print.py
@@ -2,3 +2,7 @@ print("이것은 프린트다")
 print("C언어와 달리 세미 콜론이 필수가 아니다")
 print("print()는 자체적으로 \n을 포함하고 있다")
 print("만약 이어 붙이기를 원한다면 end =을 이용하거나 콤마(,)나 + 더하기 연산자를 사용해야 한다. ")
+
+raba = "rabarata istaferium"
+
+print(raba[2:8:2][::-1])
