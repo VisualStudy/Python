7b01da3aa4b0e40b16a22c4c8a4fcc5602df07a7
지호 박 <sparksoldier@outlook.com>
2025-11-30 00:28:37 +0900

    function

--- Python/Python/turtleDrawRect.py
+++ Python/Python/turtleDrawRect.py
@@ -3,6 +3,10 @@ import turtle
 t = turtle.Turtle()
 t.shape("turtle")
 
+def move(f, a):
+    t.forward(f)
+    t.right(a)
+
 t.forward(100)
 t.right(90)
 t.forward(100)
@@ -11,5 +15,7 @@ t.forward(100)
 t.right(90)
 t.forward(100)
 t.right(90)
+
+move(200, 90)
 
 turtle.done()
