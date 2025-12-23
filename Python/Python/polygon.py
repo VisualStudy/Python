e5d2dcf3d61ef8f357090246519b13b2f12e5ae8
VisualStudy <sparksoldier@outlook.com>
2025-12-23 23:37:21 +0900

    refactoring

--- Python/Python/polygon.py
+++ Python/Python/polygon.py
@@ -1,10 +1,8 @@
-import turtle
-
-t = turtle.Turtle()
+from turtle import *
+t = Turtle()
 
 n = int(input('3~5 사이의 정수: '))
 d = int(input('10~30 사이의 큰 정수: '))
 
 def pg():
-
-turtle.done()
+    print("block")
