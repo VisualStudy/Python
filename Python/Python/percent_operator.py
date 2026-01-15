ede7c8abd2297c1703508b4c27703595101fd44b
VisualStudy <sparksoldier@outlook.com>
2026-01-15 23:19:01 +0900

    ;;

--- Python/Python/percent_operator.py
+++ Python/Python/percent_operator.py
@@ -3,5 +3,5 @@ print(a)
 b = 3 % 2
 print(b)
 print("python print 함수는 자체 내장된 \n이 작동합니다.")
-print("python print 함수는 자체 내장된 \\n이 작동합니다.")
+print("python print 함수는 자체 내장된 \\n이 작동합니다.");
 print("master TA");
